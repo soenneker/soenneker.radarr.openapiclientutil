d7a1f931621c6a4aff7c12116205e6b44424982f
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:38 -0500

    xml commenting

--- src/Soenneker.Radarr.OpenApiClientUtil/Abstract/IRadarrOpenApiClientUtil.cs
+++ src/Soenneker.Radarr.OpenApiClientUtil/Abstract/IRadarrOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Radarr.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IRadarrOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<RadarrOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
